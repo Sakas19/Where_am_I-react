ff0b1ecf7e4e57d092c3b215dd411a7bae37d546
Sarvnaz K <sarvnaz.kasaei@edu.cmeducations.se>
2023-04-23 21:22:33 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      external: ['./src/main.jsx']
+    }
+  }
+}
+
