c4ff51792842274f499db8f95235fc9c56ef4922
Sarvnaz K <sarvnaz.kasaei@edu.cmeducations.se>
2023-04-24 12:50:38 +0200

    second try to fix deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['./src/main.jsx']
+    }
+  }
 })
 
